32f5fd045389a84e170e46022c733546cf67afc7
OumaBon <mugahboniface@gmail.com>
2025-07-22 20:04:41 +0300

    product-service-model refactered

--- app/model.py
+++ app/model.py
@@ -4,6 +4,26 @@ from .import db
 
 
 
+class Brand(db.Model):
+    __tablename__="brands"
+    id  = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String, nullable=False, index=True)
+    description = db.Column(db.String, nullable=False)
+    
+    products = db.relationship("Product", back_populates='brand')
+
+
+class Category(db.Model):
+    __tablename__="categories"
+    id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String, nullable=False, index=True, unique=True)
+    parent_id = db.Column(db.String, db.ForeignKey('categories.id'), nullable=False)
+    
+    parent = db.relationship("Category", remote_side=[id], backref='subcategories')
+    subcategories = db.relationship('Category', back_populates='parent', cascade="all, delete")
+    products = db.relationship('Product', back_populates='category')
+    
+
 class Product(db.Model):
     __tablename__ = "products"
     id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
@@ -16,10 +36,10 @@ class Product(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     
-    category = db.relationship('Category', backref='products')
-    brand = db.relationship('Brand', backref='products')
-
-
+    category = db.relationship('Category', back_populates='products')
+    brand = db.relationship('Brand', back_populates='products')
+    variants = db.relationship('ProductVariant', back_populates='product', cascade="all, delete-orphan")
+    images = db.relationship('ProductImage', back_populates='product', cascade="all, delete-orphan")
 
 
 class ProductVariant(db.Model):
@@ -32,32 +52,17 @@ class ProductVariant(db.Model):
     stock = db.Column(db.Integer, nullable=False)
     price_override=db.Column(db.Numeric(10, 2),  nullable=True)
     
-    product = db.relationship('Product', backref='product_variants')
-
-
-class Brand(db.Model):
-    __tablename__="brands"
-    id  = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
-    name = db.Column(db.String, nullable=False, index=True)
-    description = db.Column(db.String, nullable=False)
-
-
-class Category(db.Model):
-    __tablename__="categories"
-    id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
-    name = db.Column(db.String, nullable=False, index=True, unique=True)
-    parent_id = db.Column(db.String, db.ForeignKey('categories.id'), nullable=False)
+    product = db.relationship('Product', backref='variants')
     
-    parent = db.relationship("Category", remote_side=[id], backref='subcategories')
 
-
-class ProductImages(db.Model):
+class ProductImage(db.Model):
     __tablename__="product_images"
     id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
     product_id = db.Column(db.String, db.ForeignKey('products.id'), nullable=False)
     image_url=db.Column(db.Text, nullable=False)
     alt_text =db.Column(db.String)
     
-    product = db.relationship('Product', backref='product_images')
+    product = db.relationship('Product', back_populates='images')
+    
 
     
