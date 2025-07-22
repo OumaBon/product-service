c346f037e3a9c5aad6de18935566d25e5401c0ba
OumaBon <mugahboniface@gmail.com>
2025-07-22 23:21:45 +0300

    updated product apis

--- app/api_v1/product.py
+++ app/api_v1/product.py
@@ -5,28 +5,41 @@ from marshmallow import ValidationError
 
 from . import api
 from ..schema import ProductSchema
-from ..model import Product
+from ..model import Product, Category,Brand
 from .. import db
+
 
 
 @api.route('/product', methods=["POST"])
 def new_product():
     try:
         data = request.get_json()
+        category =Category.query.filter_by(slug=data.get('category_slug')).first()
+        if not category:
+            return jsonify({"error": "Category not Found"}), 404
+        
+        brand = Brand.query.filter_by(name=data.get('brand_name')).first()
+        if not brand:
+            return jsonify({"error": "Brand not found"}), 404
+        
+        data['category_id'] = category.id 
+        data['brand_id'] = brand.id 
+        
         schema = ProductSchema()
         product = schema.load(data, session=db.session)
+        
         db.session.add(product)
         db.session.commit()
         
-        return jsonify({schema.dump(product)}),201
-    
+        return jsonify(schema.dump(product)), 201
     except ValidationError as err:
         return jsonify({"errors": err.messages}), 400
     
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-     
+
+
 
 @api.route('/product', methods=["GET"])
 def get_products():
