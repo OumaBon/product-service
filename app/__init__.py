fd8b836841ec96ead0e337a2c17304a1feb12088
OumaBon <mugahboniface@gmail.com>
2025-07-22 14:57:32 +0300

    Flask Application creation function

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+
+from config import config
+
+
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+cors = CORS()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    cors.init_app(app, origins=["http://localhost:3000"])
+    
+    
+    return app 
